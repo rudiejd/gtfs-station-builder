de3e1d5e0bbeb4e804860ab5d16ef0f5ffc1cded
JD <jd@rudiejd.io>
2026-03-03 19:30:55 -0500

    feat: show sub-graph of pathways connected to one node on selection of that node

--- src/interfaces/VisEdge.tsx
+++ src/interfaces/VisEdge.tsx
@@ -1,21 +1,21 @@
 import Pathway from "./Pathway";
 
 export default interface VisEdge {
-    id: string,
-    from: string,
-    to: string,
-    color: {
-        color: string,
-        highlight: string
-    },
-    arrows: {
-        to: boolean,
-        from: boolean
+	id: string,
+	from: string,
+	to: string,
+	color: {
+		color: string,
+		highlight: string
+	},
+	arrows: {
+		to: boolean,
+		from: boolean
 	},
 	smooth: {
 		type: string,
 		roundness: number
 	},
 	label: string,
-    pathway: Pathway
+	pathway: Pathway
 }
